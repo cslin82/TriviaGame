65ff59dbd3ccce5a26644684fbc1d7a48a0dbc7a
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-03-20 03:06:31 -0400

    Trim whitespace, hide timer, show correct on timeup

--- assets/javascript/app-advanced.js
+++ assets/javascript/app-advanced.js
@@ -5,7 +5,7 @@ var newUL;
 var newLI;
 var qaDiv;
 
-var timerMax = 15;
+var timerMax = 10;
 var timer = timerMax;
 var timerfrac;
 var counterID;
@@ -22,55 +22,42 @@ const gameQuestions = [
     {
         q: "In what year was the Smithsonian Institution established?",
         a: ["1846", "1861", "1934", "1776"],
-        correct: 0 // array index of a
     },
     {
         q: "Who was the founding donor the Smithsonian is named after?",
         a: ["James Smithson", "Robert Smithson", "John Smith", "Samuel Smithsonian"],
-        correct: 0 // array index of a
     },
     {
         q: "Which of these is National things is NOT a Smithsonian museum?",
         a: ["National Gallery of Art", "National Zoo", "National Museum of African American History and Culture", "National Museum of the American Indian"],
-        correct: 0 // array index of a
     },
-
     {
         q: "Which of these National things is a Smithsonian museum?",
         a: ["National Postal Museum", "National Archives", "National Museum of Women in the Arts", "Washington Nationals"],
-        correct: 0 // array index of a
     },
-
     {
         q: "At which Washington-area airport is the Steven F. Udvar-Hazy Center located?",
         a: ["Washington Dulles International Airport", "Ronald Reagan Washington National Airport", "Baltimore–Washington International Airport", "Andrews Air Force Base"],
-        correct: 0 // array index of a
     },
-
     {
         q: "At which museum would you find the Greensboro Lunch Counter?",
         a: ["National Museum of American History", "National Museum of African American History and Culture", "Smithsonian American Art Museum", "Arthur M. Sackler Gallery"],
-        correct: 0 // array index of a
     },
     {
         q: "Which of these Smithsonian museums is located on the National Mall?",
         a: ["Freer Gallery of Art", "Renwick Gallery", "National Postal Museum", "Anacostia Community Museum"],
-        correct: 0 // array index of a
     },
     {
         q: "Which of these Smithsonian museums is the newest?",
         a: ["National Museum of African American History and Culture", "National Air and Space Museum", "Hirshhorn Museum and Sculpture Garden", "National Museum of Natural History"],
-        correct: 0 // array index of a
     },
     {
         q: "Which Space Shuttle orbiter would you find at the Steven F. Udvar-Hazy Center?",
         a: ["Discovery", "Atlantis", "Endeavour", "Enterprise"],
-        correct: 0 // array index of a
     },
     {
         q: "What government agency formerly occupied the current site of the Smithsonian American Art Museum and the National Portrait Gallery?",
         a: ["Patent Office", "Department of War", "National Endowment for the Arts", "Department of Education"],
-        correct: 0 // array index of a
     },
 ]
 
@@ -78,7 +65,6 @@ const gameQuestions = [
 // https://stackoverflow.com/questions/3746725/create-a-javascript-array-containing-1-n https://stackoverflow.com/a/20066663
 var answerDeck = [0, 1, 2, 3];
 var questionDeck = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 
 // inclusive
 function randomInt(min, max) {
@@ -96,10 +82,58 @@ function shuffleFY(array) {
     }
 }
 
-
 // Scoring function
+function gameEnd() {
+    alert( 'correct: ' + correct + ' and incorrect: ' + incorrect + ' for a percentage of ' + Math.round(100*(correct/(correct+incorrect)))+ '%' );
+}
+
+// Pick a question and show it 
+function newQuestion(qIndex) {
+    currentQuestion = Object.assign({}, gameQuestions[qIndex]);
+    $('#questionH2').text(currentQuestion.q);
+    shuffleFY(answerDeck);
+    console.log('answerDeck: '+ answerDeck);
+
+    // could do this the other way***
+    for (var indexA = 0; indexA < 4; indexA++) {
+        var myIndex = answerDeck[indexA];
+        $('#answer-' + (indexA+1) + '>h4').text(currentQuestion.a[myIndex]);
+        $('#answer-' + (indexA+1)).data("value", myIndex);
+        $('#answer-' + (indexA+1)).removeClass("list-group-item-success list-group-item-danger");
+    }
+
+    // and reset, show and start the timer
+    timer = timerMax;
+    $('#timerbar').css("width", "100%");
+    $('#timerrow').show();
+    console.log('timer started');
+    if (!timerOn) {
+        $('#timerspan').text(timer);
+        timerOn = true;
+        counterID = setInterval(function () {
+            timer--;
+            console.log(timer);
+
+            $('#timerspan').text(timer);
+            timerfrac = Math.round((timer / timerMax) * 100);
+            $('#timerbar').css("width", timerfrac + "%");
+
+            if (timer <= 0) {
+                
+                $('#answer-' + (answerDeck.indexOf(0)+1)).addClass("list-group-item-success");
+                alert('time up');
+
+                
+
+                clearInterval(counterID);
+                timerOn = false;
+                return;
+            }
+        }, 1000);
+    }
 
 
+}
 
 $(document).ready(function() {
 
@@ -134,21 +168,11 @@ $(document).ready(function() {
     $('#questionanswer').append(newUL);
     console.log('ul appended to #questionanswer');
 
-    // Pick a question and show it 
-    function newQuestion(qIndex) {
-        currentQuestion = Object.assign({}, gameQuestions[qIndex]);
-        $('#questionH2').text(currentQuestion.q);
-        shuffleFY(answerDeck);
-        console.log('answerDeck: '+ answerDeck);
-        for (var indexA = 0; indexA < 4; indexA++) {
-            var myIndex = answerDeck[indexA];
-            $('#answer-' + (indexA+1) + '>h4').text(currentQuestion.a[myIndex]);
-            $('#answer-' + (indexA+1)).data("value", myIndex);
-        }
-    }
+    // Pick a question and show it source
+    
     
 
-     // Click listeners
+    // Click listeners
     $('.answers').click(function() {
         console.log('clicked ' + $(this).attr('id'));
         console.log('and data.value is ' + $(this).data('value'));
@@ -156,6 +180,8 @@ $(document).ready(function() {
         if (timerOn) {
             clearInterval(counterID);
             timerOn = false;
+            $('#timerrow').hide();
+            console.log('timer stopped on answer, timer hidden');
 
         }
         if ($(this).data('value') === 0) {
