b01d1aa09240c668c1cec96ce90bd0588e38b8e5
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-03-20 11:50:43 -0400

    Add SI logo; add start/restart button; add game stats

--- assets/javascript/app-advanced.js
+++ assets/javascript/app-advanced.js
@@ -13,8 +13,8 @@ var gameActive = false;
 var currentQuestion;
 var correct = 0;
 var incorrect = 0;
-
-
+var omit = 0;
+var roundNumber = 0;
 
 // Game questions for Smithsonian Institution theme
 const gameQuestions = [
@@ -63,7 +63,7 @@ const gameQuestions = [
 // easiest to hard code these, could generalize with for loop or fancy array.apply() methods
 // https://stackoverflow.com/questions/3746725/create-a-javascript-array-containing-1-n https://stackoverflow.com/a/20066663
 var answerDeck = [0, 1, 2, 3];
-var questionDeck = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var questionDeck = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // inclusive
 function randomInt(min, max) {
@@ -81,99 +81,149 @@ function shuffleFY(array) {
     }
 }
 
-// Scoring function
-function gameEnd() {
-    alert( 'correct: ' + correct + ' and incorrect: ' + incorrect + ' for a percentage of ' + Math.round(100*(correct/(correct+incorrect)))+ '%' );
-}
 
-// Pick a question and show it 
-function newQuestion(qIndex) {
-    currentQuestion = Object.assign({}, gameQuestions[qIndex]);
-    $('#questionH2').text(currentQuestion.q);
 
-    shuffleFY(answerDeck);
-    for (var indexA = 0; indexA < 4; indexA++) {
-        var myIndex = answerDeck[indexA];
-        $('#answer-' + (indexA+1) + '>h4').text(currentQuestion.a[myIndex]);
-        $('#answer-' + (indexA+1)).data("value", myIndex);
-        $('#answer-' + (indexA+1)).removeClass("list-group-item-success list-group-item-danger");
-    }
 
-    // and reset, show and start the timer
-    gameActive = true;
-    timer = timerMax;
-    $('#timerbar').css("width", "100%");
-    $('#timerrow').show();
-    if (!timerOn) {
-        $('#timerspan').text(timer);
-        timerOn = true;
-        counterID = setInterval(function () {
-            timer--;
+$(document).ready(function () {
 
-            $('#timerspan').text(timer);
-            timerfrac = Math.round((timer / timerMax) * 100);
-            $('#timerbar').css("width", timerfrac + "%");
+    // function declarations
 
-            if (timer <= 0) {
-                
-                $('#answer-' + (answerDeck.indexOf(0)+1)).addClass("list-group-item-success");
-                alert('time up');
+    // Pick a question and show it 
+    function newQuestion(qIndex) {
+        // currentQuestion = Object.assign({}, gameQuestions[qIndex]);
+        currentQuestion = gameQuestions[qIndex];
+        $('#questionH2').text(currentQuestion.q);
 
-                clearInterval(counterID);
-                timerOn = false;
-                return;
-            }
-        }, 1000);
-    }
-}
-
-$(document).ready(function() {
-
-    console.log( "ready!" );
-    qaDiv = $('#questionanswer');
-
-    // Dynamically generate question and answer elements, timer progress bar
-    $('#questionanswer').append($('<h2>').attr("id","questionH2"));
-    
-    newUL = $('<ul>');
-    newUL.addClass("list-group");
-    
-    for (var indexA = 0; indexA < 4; indexA++) {
-        newLI = $('<li>');
-        newLI.addClass("list-group-item text-center answers");
-        newLI.attr("id", "answer-" + (indexA+1));
-        newLI.append($('<h4>'));
-        newUL.append(newLI);
-    }
-    $('#questionanswer').append(newUL);
-
-    // Click listeners
-    $('.answers').click(function() {
-        if (gameActive) {
-            if (timerOn) {
-                clearInterval(counterID);
-                timerOn = false;
-                $('#timerrow').hide();
-                console.log('timer stopped on answer, timer hidden');
-            }
-            if ($(this).data('value') === 0) {
-                $(this).addClass("list-group-item-success");
-                alert('correct');
-            } else {
-                $(this).addClass("list-group-item-danger");
-                alert('incorrect');
-            }
-            gameActive = false;
+        shuffleFY(answerDeck);
+        for (var indexA = 0; indexA < 4; indexA++) {
+            var myIndex = answerDeck[indexA];
+            $('#answer-' + (indexA + 1) + '>h4').text(currentQuestion.a[myIndex]);
+            $('#answer-' + (indexA + 1)).data("value", myIndex);
+            $('#answer-' + (indexA + 1)).removeClass("list-group-item-success list-group-item-danger");
         }
 
+        // and reset, show and start the timer
+        gameActive = true;
+        timer = timerMax;
+        $('#timerbar').css("width", "100%");
+        $('#timerrow').show();
+        if (!timerOn) {
+            $('#timerspan').text(timer);
+            timerOn = true;
+            counterID = setInterval(function () {
+                timer--;
 
-        
-        
+                $('#timerspan').text(timer);
+                timerfrac = Math.round((timer / timerMax) * 100);
+                $('#timerbar').css("width", timerfrac + "%");
 
+                if (timer <= 0) {
 
+                    $('#answer-' + (answerDeck.indexOf(0) + 1)).addClass("list-group-item-success");
+                    alert('time up');
+                    $('#timerrow').hide();
+                    omit++;
+
+                    questiondone();
+
+                    clearInterval(counterID);
+                    timerOn = false;
+                    return;
+                }
+            }, 1000);
+        }
+    }
+
+    function questiondone() {
+        if (roundNumber >= (gameQuestions.length - 1)) {
+            setTimeout(gameEnd, 1000);
+            return;
+        } else {
+
+            roundNumber++;
+            setTimeout(function () {
+                newQuestion(questionDeck[roundNumber]);
+            }, 3000);
+        }
+    }
+// Scoring function
+function gameEnd() {
+    // alert( 'correct: ' + correct + ' and incorrect: ' + incorrect + ' for a percentage of ' + Math.round(100*(correct/(correct+incorrect+omit)))+ '%' );
+    qaDiv.empty();
+    // $('#timerrow').hide();
+
+    qaDiv.append($('<h2>').text('Game over!'));
+    qaDiv.append($('<h3>').text('You got ' + correct + ' correct, missed ' + incorrect + ' and did not answer ' + omit + ' questions.'));
+    qaDiv.append($('<h3>').text('Your score is ' + Math.round(100 * (correct / (correct + incorrect + omit))) + '%'));
+    qaDiv.append('<button class="btn btn-primary btn-lg btn-block" id="starter-btn">Start a new game</button>');
+    $('#starter-btn').click(function () {
+        newGame();
     });
-    
-    newQuestion(randomInt(0,10));
+}
+    console.log("ready!");
+    qaDiv = $('#questionanswer');
 
-// end jQuery
+    function newGame() {
+        qaDiv.empty();
+        // Dynamically generate question and answer elements, timer progress bar
+        qaDiv.append($('<h2>').attr("id", "questionH2"));
+
+        newUL = $('<ul>');
+        newUL.addClass("list-group");
+
+        for (var indexA = 0; indexA < 4; indexA++) {
+            newLI = $('<li>');
+            newLI.addClass("list-group-item text-center answers");
+            newLI.attr("id", "answer-" + (indexA + 1));
+            newLI.append($('<h4>'));
+            newUL.append(newLI);
+        }
+        qaDiv.append(newUL);
+
+        // Click listeners
+        $('.answers').click(function () {
+            if (gameActive) {
+                if (timerOn) {
+                    clearInterval(counterID);
+                    timerOn = false;
+                    $('#timerrow').hide();
+                    console.log('timer stopped on answer, timer hidden');
+                }
+                if ($(this).data('value') === 0) {
+                    $(this).addClass("list-group-item-success");
+                    correct++;
+                    alert('correct');
+                } else {
+                    $(this).addClass("list-group-item-danger");
+                    $('#answer-' + (answerDeck.indexOf(0) + 1)).addClass("list-group-item-success");
+                    incorrect++;
+                    alert('incorrect');
+                }
+                gameActive = false;
+
+                // *** need to move this out of this click handler so it handles non-answers
+                questiondone();
+            }
+            // $('#timerrow').show();
+
+
+        });
+
+
+
+        shuffleFY(questionDeck);
+
+        newQuestion(questionDeck[roundNumber]);
+
+        // end newGame
+    }
+
+    $('#timerrow').hide();
+
+    $('#starter').click(function () {
+        newGame();
+    });
+
+
+    // end jQuery
 });
